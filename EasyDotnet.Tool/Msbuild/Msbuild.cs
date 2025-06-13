66cd64d2c53d1b2a889be2268b675388a3833762
Gustav Eikaas <46537983+GustavEikaas@users.noreply.github.com>
2025-06-13 18:28:07 +0200

    fix: add restore target to build request (#15)

--- EasyDotnet.Tool/Msbuild/Msbuild.cs
+++ EasyDotnet.Tool/Msbuild/Msbuild.cs
@@ -1,15 +1,11 @@
 using System.Collections.Generic;
-
 using Microsoft.Build.Evaluation;
 using Microsoft.Build.Execution;
 using Microsoft.Build.Framework;
 
 namespace EasyDotnet.Msbuild;
 
-public record BuildResult(
-  Microsoft.Build.Execution.BuildResult Result,
-  List<BuildMessage> Messages
-);
+public record BuildResult(Microsoft.Build.Execution.BuildResult Result, List<BuildMessage> Messages);
 
 public class Msbuild
 {
@@ -18,7 +14,7 @@ public class Msbuild
     var properties = new Dictionary<string, string?> { { "Configuration", configuration } };
 
     var pc = new ProjectCollection(properties);
-    var buildRequest = new BuildRequestData(targetPath, properties, null, ["Build"], null);
+    var buildRequest = new BuildRequestData(targetPath, properties, null, ["Restore", "Build"], null);
     var logger = new InMemoryLogger();
 
     var parameters = new BuildParameters(pc) { Loggers = [logger] };
@@ -29,14 +25,7 @@ public class Msbuild
   }
 }
 
-public sealed record BuildMessage(
-  string Type,
-  string FilePath,
-  int LineNumber,
-  int ColumnNumber,
-  string Code,
-  string? Message
-);
+public sealed record BuildMessage(string Type, string FilePath, int LineNumber, int ColumnNumber, string Code, string? Message);
 
 public class InMemoryLogger : ILogger
 {
@@ -47,28 +36,8 @@ public class InMemoryLogger : ILogger
 
   public void Initialize(IEventSource eventSource)
   {
-    eventSource.ErrorRaised += (sender, args) =>
-      Messages.Add(
-        new BuildMessage(
-          "error",
-          args.File,
-          args.LineNumber,
-          args.ColumnNumber,
-          args.Code,
-          args?.Message
-        )
-      );
-    eventSource.WarningRaised += (sender, args) =>
-      Messages.Add(
-        new BuildMessage(
-          "warning",
-          args.File,
-          args.LineNumber,
-          args.ColumnNumber,
-          args.Code,
-          args?.Message
-        )
-      );
+    eventSource.ErrorRaised += (sender, args) => Messages.Add(new BuildMessage("error", args.File, args.LineNumber, args.ColumnNumber, args.Code, args?.Message));
+    eventSource.WarningRaised += (sender, args) => Messages.Add(new BuildMessage("warning", args.File, args.LineNumber, args.ColumnNumber, args.Code, args?.Message));
   }
 
   public void Shutdown() { }
